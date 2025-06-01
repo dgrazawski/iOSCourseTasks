8b5dcd189961811831d8296b2925857225c21030
dgrazawski <62799954+dgrazawski@users.noreply.github.com>
2025-06-01 16:58:39 +0200

    added task 2 in part 4 of swift language

--- swift_language/part_4/task2.playground/Contents.swift
+++ swift_language/part_4/task2.playground/Contents.swift
@@ -1,3 +1,58 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+protocol Shape {
+    func area() -> Double
+    func perimeter() -> Double
+}
+
+
+class Circle: Shape {
+    let radius: Double
+    
+    init(radius: Double) {
+        self.radius = radius
+    }
+    
+    func area() -> Double {
+        return Double.pi * radius * radius
+    }
+    
+    func perimeter() -> Double {
+        return 2 * Double.pi * radius
+    }
+}
+
+
+class Rectangle: Shape {
+    let sideA: Double
+    let sideB: Double
+    
+    init(sideA: Double, sideB: Double) {
+        self.sideA = sideA
+        self.sideB = sideB
+    }
+    
+    func area() -> Double {
+        return sideA * sideB
+    }
+    
+    func perimeter() -> Double {
+        return 2 * (sideA + sideB)
+    }
+}
+
+
+func generateShape() -> some Shape {
+    return Circle(radius: 5)
+}
+
+func calculateShapeDetails(_ shape: any Shape) -> (Double, Double) {
+    return (shape.area(), shape.perimeter())
+}
+
+
+let rect1 = Rectangle(sideA: 5, sideB: 15)
+let circ1 = generateShape()
+
+print(calculateShapeDetails(rect1))
+print(calculateShapeDetails(circ1))
