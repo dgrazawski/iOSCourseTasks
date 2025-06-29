3b2b7e9c8036bc55b01ff8fa18c595228c1e87df
dgrazawski <62799954+dgrazawski@users.noreply.github.com>
2025-06-29 17:35:00 +0200

    init for gallery app

---
+++ UIKit/PhotoGalleryApp/PhotoGalleryApp/GalleryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GalleryViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Dawid Grazawski on 29/06/2025.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
