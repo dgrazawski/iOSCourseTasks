34b16e3683bfb1867ca3f94897da301280efae97
dgrazawski <62799954+dgrazawski@users.noreply.github.com>
2025-08-04 21:46:27 +0200

    finished task 6 of combine

--- SwiftUI/combine_6.playground/Contents.swift
+++ SwiftUI/combine_6.playground/Contents.swift
@@ -1,4 +1,14 @@
 import UIKit
 import Combine
 
+let intsSubject = PassthroughSubject<Int, Never>()
 
+
+let cancellable = intsSubject
+    .flatMap { Just($0 * $0) }
+    .sink { print($0) }
+
+
+for number in 0...10 {
+    intsSubject.send(number)
+}
